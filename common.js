0b01a407728022e61ef45d3a2893c089d34aaa38
sarachs <77602200+sarachs@users.noreply.github.com>
2021-04-15 23:01:55 +0800

    Update common.js

--- common.js
+++ common.js
@@ -33,7 +33,7 @@ function randomColor() {
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
-    return 'rgb(' + r + ',' + g + ',' + b + ')';
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
 
 
