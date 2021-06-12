115933a4076d1cf0d1381e05316f38f6cb3029f2
sarachs <77602200+sarachs@users.noreply.github.com>
2021-06-12 16:13:29 +0800

    Update String.js

--- 自定义工具函数/String.js
+++ 自定义工具函数/String.js
@@ -1,4 +1,22 @@
-// 文本截断, 多余文本显示...  
-function truncate(str, size) {
-    return str.slice(0, size) + "...";
+/* 
+1. 字符串倒序: reverseString(str)  生成一个倒序的字符串
+*/
+function reverseString(str) {
+	// return str.split('').reverse().join('')
+    // return [...str].reverse().join('')
+    return Array.from(str).reverse().join('')
+}
+
+/* 
+2. 字符串是否是回文: palindrome(str) 如果给定的字符串是回文，则返回 true ；否则返回 false
+*/
+function palindrome(str) {
+    return str === reverseString(str)
+}
+
+/* 
+3. 截取字符串: truncate(str, num) 如果字符串的长度超过了num, 截取前面num长度部分, 并以...结束
+*/
+function truncate(str, num) {
+	return str.length > num ? str.slice(0, num) + '...' : str
 }
